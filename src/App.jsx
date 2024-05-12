45bef6aba76df260ebb4f3736513ae0dd0bdd2ae
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2024-05-13 04:24:23 +1000

    Adding d3 example (not working yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as d3 from 'd3'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Charts from './Charts/Charts'
+
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    const dataURL = "http://d3js-in-action-third-edition.github.io/hosted-data/apis/front_end_frameworks.json";
+    d3.json(dataURL).then(data => {
+      setData(data);
+      setLoading(false);
+    })
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {loading && <div className="loading">Loading...</div>}
+    {!loading && <Charts />}
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 	base: "/d3js/",
+	cors: true,
 })
